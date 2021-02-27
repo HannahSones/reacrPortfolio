bcfd081abb327357ed50d86a870e7b95dcce270c
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2021-02-27 22:09:52 +0000

    test

--- src/routes/routing.js
+++ src/routes/routing.js
@@ -1,4 +1,5 @@
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
+import { Route, Switch } from 'react-router';
 import Menu from '../components/Menu/menu';
 import Home from '../components/HomePage/home';
 import Portfolio from '../components/PortfolioPage/portfolio';
@@ -7,11 +8,13 @@ import Footer from '../components/Footer/footer';
 
 const Routing = () => {
   return (
-    <Router basename='/'>
+    <Router basename={process.env.PUBLIC_URL}>
       <Menu />
+      <Switch>
       <Route exact path='/' component={Home}/>
       <Route exact path='/portfolio' component={Portfolio}/>
       <Route exact path='/contact' component={Contact}/>
+      </Switch>
       <Footer />
     </Router>
   )
