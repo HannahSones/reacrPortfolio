6af56a1ea4d4c127b2529fa9cb725c0620a7d683
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2021-02-28 11:02:57 +0000

    Adding functionality to portfolio page

--- src/components/Menu/menu.js
+++ src/components/Menu/menu.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../style.css';
 import './menu.css';
 import Navigation from '../Navigation/navigation';
@@ -8,13 +9,17 @@ function Menu() {
     <div className="menu fixed-top">
       <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            <img
+          <a className="navbar-brand">
+          <Link to="/">
+          <span>
+          <img
               src={`${process.env.PUBLIC_URL}/assets/Logo_transparent.png`}
               height="50"
               alt="Logo"
               loading="lazy"
             />
+          </span>
+        </Link>
           </a>
           <button
             className="navbar-toggler"
--- src/components/PortfolioPage/portfolio.js
+++ src/components/PortfolioPage/portfolio.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
-import "../style.css";
-import "./portfolio.css";
-import AOS from "aos";
-import "aos/dist/aos.css";
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import '../style.css';
+import './portfolio.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function Portfolio() {
   useEffect(() => {
@@ -60,24 +61,142 @@ function Portfolio() {
                   </p>
                   <a
                     href="https://mywardrobeproject.herokuapp.com/"
-                    className="btn"
+                    className="btn projectBtn"
                     target="_blank"
                   >
                     View app
                   </a>
                   <a
                     href="https://github.com/HannahSones/myWardrobe"
-                    className="btn"
+                    className="btn projectBtn"
                     target="_blank"
                   >
                     View repo
                   </a>
                 </div>
                 <div className="card-footer">
-                  <i class="fab fa-html5"></i>
-                  <i class="fab fa-css3-alt"></i>
-                  <i class="fab fa-js"></i>
-                  </div>
+                  <i className="fab fa-js fa-2x"></i>
+                  <i className="fab fa-node fa-2x"></i>
+                  <i className="fas fa-database fa-2x"></i>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card h-100">
+                <img
+                  src={`${process.env.PUBLIC_URL}/assets/Projects/employeeDirectory.png`}
+                  className="card-img-top"
+                  alt="An application to find employees in a database"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Employee Directory</h5>
+                  <hr />
+                  <p className="card-text">
+                    An employee directory built using React, so that a user can
+                    view their entire employee directory at once and have quick
+                    access to information.
+                  </p>
+                  <a
+                    href="https://hannahsones.github.io/employeedirectory/"
+                    className="btn projectBtn"
+                    target="_blank"
+                  >
+                    View app
+                  </a>
+                  <a
+                    href="https://github.com/HannahSones/employeedirectory"
+                    className="btn projectBtn"
+                    target="_blank"
+                  >
+                    View repo
+                  </a>
+                </div>
+                <div className="card-footer">
+                  <i class="fab fa-react fa-2x"></i>
+                  <i className="fab fa-node fa-2x"></i>
+                  <i className="fas fa-database fa-2x"></i>
+                  <i className="fab fa-js fa-2x"></i>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card h-100">
+                <img
+                  src={`${process.env.PUBLIC_URL}/assets/Projects/WeatherDashboard.png`}
+                  className="card-img-top"
+                  alt="An application for viewing the weather"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Weather Dashboard</h5>
+                  <hr />
+                  <p className="card-text">
+                    A dynamic weather dashboard utilising the Open Weather API
+                    and built with jQuery, HTML and Bootstrap.
+                  </p>
+                  <a
+                    href="https://hannahsones.github.io/WeatherDashboard/"
+                    className="btn projectBtn"
+                    target="_blank"
+                  >
+                    View app
+                  </a>
+                  <a
+                    href="https://github.com/HannahSones/WeatherDashboard"
+                    className="btn projectBtn"
+                    target="_blank"
+                  >
+                    View repo
+                  </a>
+                </div>
+                <div className="card-footer">
+                  <i className="fab fa-html5 fa-2x"></i>
+                  <i className="fab fa-css3-alt fa-2x"></i>
+                  <i className="fab fa-js fa-2x"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            className="row row-cols-1 row-cols-md-3 g-4"
+            data-aos="fade-up"
+            data-aos-offset="200"
+            data-aos-duration="2000"
+          >
+            <div className="col">
+              <div className="card h-100">
+                <img
+                  src={`${process.env.PUBLIC_URL}/assets/Projects/budgetTracker.png`}
+                  className="card-img-top"
+                  alt="Budget tracker"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Budget Tracker</h5>
+                  <hr />
+                  <p className="card-text">
+                    A Budget Tracker application using PWA functionality with a
+                    manifest and service worker file to allow for offline
+                    access.
+                  </p>
+                  <a
+                    href="https://thawing-reaches-04479.herokuapp.com/"
+                    className="btn projectBtn"
+                    target="_blank"
+                  >
+                    View app
+                  </a>
+                  <a
+                    href="https://github.com/HannahSones/budgetTracker"
+                    className="btn projectBtn"
+                    target="_blank"
+                  >
+                    View repo
+                  </a>
+                </div>
+                <div className="card-footer">
+                  <i className="fas fa-database fa-2x"></i>
+                  <i className="fab fa-node fa-2x"></i>
+                  <i className="fab fa-js fa-2x"></i>
+                </div>
               </div>
             </div>
             <div className="col">
@@ -97,60 +216,60 @@ function Portfolio() {
                   </p>
                   <a
                     href="https://pavn93.github.io/Never-Miss-A-Beat/"
-                    className="btn"
+                    className="btn projectBtn"
                     target="_blank"
                   >
                     View app
                   </a>
                   <a
                     href="https://github.com/HannahSones/Never-Miss-A-Beat"
-                    className="btn"
+                    className="btn projectBtn"
                     target="_blank"
                   >
                     View repo
                   </a>
                 </div>
                 <div className="card-footer">
-                  <i class="fab fa-html5"></i>
-                  <i class="fab fa-css3-alt"></i>
-                  <i class="fab fa-js"></i>
-                  </div>
+                  <i className="fab fa-html5 fa-2x"></i>
+                  <i className="fab fa-css3-alt fa-2x"></i>
+                  <i className="fab fa-js fa-2x"></i>
+                </div>
               </div>
             </div>
             <div className="col">
               <div className="card h-100">
                 <img
-                  src={`${process.env.PUBLIC_URL}/assets/Projects/WeatherDashboard.png`}
+                  src={`${process.env.PUBLIC_URL}/assets/Projects/workoutTracker.png`}
                   className="card-img-top"
-                  alt="An application for viewing the weather"
+                  alt="An app to generate random passwords"
                 />
                 <div className="card-body">
-                  <h5 className="card-title">Weather Dashboard</h5>
+                  <h5 className="card-title">Fitness Tracker</h5>
                   <hr />
                   <p className="card-text">
-                    A dynamic weather dashboard utilising the Open Weather API
-                    and built with jQuery, HTML and Bootstrap.
+                    A workout tracker application that uses a Mongo Database to
+                    allow users to keep track of their workout regime.
                   </p>
                   <a
-                    href="https://hannahsones.github.io/WeatherDashboard/"
-                    className="btn"
+                    href="https://afternoon-river-94265.herokuapp.com/?id=601d654618de7d0015c3788d"
+                    className="btn projectBtn"
                     target="_blank"
                   >
                     View app
                   </a>
                   <a
-                    href="https://github.com/HannahSones/WeatherDashboard"
-                    className="btn"
+                    href="https://github.com/HannahSones/WorkoutTracker"
+                    className="btn projectBtn"
                     target="_blank"
                   >
                     View repo
                   </a>
                 </div>
                 <div className="card-footer">
-                  <i class="fab fa-html5"></i>
-                  <i class="fab fa-css3-alt"></i>
-                  <i class="fab fa-js"></i>
-                  </div>
+                  <i className="fas fa-database fa-2x"></i>
+                  <i className="fab fa-node fa-2x"></i>
+                  <i className="fab fa-js fa-2x"></i>
+                </div>
               </div>
             </div>
           </div>
@@ -176,24 +295,24 @@ function Portfolio() {
                   </p>
                   <a
                     href="https://dry-inlet-47678.herokuapp.com/"
-                    className="btn"
+                    className="btn projectBtn"
                     target="_blank"
                   >
                     View app
                   </a>
                   <a
                     href="https://github.com/HannahSones/burger"
-                    className="btn"
+                    className="btn projectBtn"
                     target="_blank"
                   >
                     View repo
                   </a>
                 </div>
                 <div className="card-footer">
-                  <i class="fab fa-html5"></i>
-                  <i class="fab fa-css3-alt"></i>
-                  <i class="fab fa-js"></i>
-                  </div>
+                  <i className="fas fa-database fa-2x"></i>
+                  <i className="fab fa-css3-alt fa-2x"></i>
+                  <i className="fab fa-js fa-2x"></i>
+                </div>
               </div>
             </div>
             <div className="col">
@@ -212,24 +331,24 @@ function Portfolio() {
                   </p>
                   <a
                     href="https://hannahsones.github.io/WorkDayScheduler/"
-                    className="btn"
+                    className="btn projectBtn"
                     target="_blank"
                   >
                     View app
                   </a>
                   <a
                     href="https://github.com/HannahSones/WorkDayScheduler"
-                    className="btn"
+                    className="btn projectBtn"
                     target="_blank"
                   >
                     View repo
                   </a>
                 </div>
                 <div className="card-footer">
-                  <i class="fab fa-html5"></i>
-                  <i class="fab fa-css3-alt"></i>
-                  <i class="fab fa-js"></i>
-                  </div>
+                  <i className="fab fa-html5 fa-2x"></i>
+                  <i className="fab fa-css3-alt fa-2x"></i>
+                  <i className="fab fa-js fa-2x"></i>
+                </div>
               </div>
             </div>
             <div className="col">
@@ -243,32 +362,43 @@ function Portfolio() {
                   <h5 className="card-title">Password Generator</h5>
                   <hr />
                   <p className="card-text">
-                  A simple application that allows a user to generate a random password based on criteria they've selected such as length and character type.
+                    A simple application that allows a user to generate a random
+                    password based on criteria they've selected such as length
+                    and character type.
                   </p>
                   <a
                     href="https://hannahsones.github.io/PasswordGenerator/"
-                    className="btn"
+                    className="btn projectBtn"
                     target="_blank"
                   >
                     View app
                   </a>
                   <a
                     href="https://github.com/HannahSones/PasswordGenerator"
-                    className="btn"
+                    className="btn projectBtn"
                     target="_blank"
                   >
                     View repo
                   </a>
                 </div>
-                  <div className="card-footer">
-                  <i class="fab fa-html5"></i>
-                  <i class="fab fa-css3-alt"></i>
-                  <i class="fab fa-js"></i>
-                  </div>
+                <div className="card-footer">
+                  <i className="fab fa-html5 fa-2x"></i>
+                  <i className="fab fa-css3-alt fa-2x"></i>
+                  <i className="fab fa-js fa-2x"></i>
+                </div>
               </div>
             </div>
           </div>
         </div>
+      </div>
+
+      <div className="contactMe">
+        <h4>Ready to see how I can help you? <span>Get in touch</span></h4>
+        <Link to="/contact">
+          <span>
+            <button className="btn contactBtn">Contact me</button>
+          </span>
+        </Link>
       </div>
     </section>
   );
